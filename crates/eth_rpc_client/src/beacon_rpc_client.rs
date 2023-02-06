d92e6726a1c34abd069768f13526a68ba70fbd94
Thomas Braun <braunth@oregonstate.edu>
2023-02-06 10:44:09 -0500

    add debugging

--- crates/eth_rpc_client/src/beacon_rpc_client.rs
+++ crates/eth_rpc_client/src/beacon_rpc_client.rs
@@ -380,7 +380,7 @@ impl BeaconRPCClient {
 
 		let v: Value = serde_json::from_str(light_client_update_json_str)?;
 
-		let attested_header_json_str = serde_json::to_string(&v[0]["data"]["attested_header"])?;
+		let attested_header_json_str = serde_json::to_string(&v[0]["data"]["attested_header"]["beacon"])?;
 		let attested_header: BeaconBlockHeader = serde_json::from_str(&attested_header_json_str)?;
 
 		let mut signature_slot = attested_header.slot + 1;
