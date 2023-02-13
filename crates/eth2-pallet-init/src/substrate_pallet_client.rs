e22c33bcb1f325cbb268d49d94d4db7d3f20dc0a
Thomas Braun <braunth@oregonstate.edu>
2023-02-13 12:45:11 -0500

    wip

--- crates/eth2-pallet-init/src/substrate_pallet_client.rs
+++ crates/eth2-pallet-init/src/substrate_pallet_client.rs
@@ -73,7 +73,7 @@ impl EthClientPallet {
 		};
 
 		let fields = vec![
-			("typed_chain_id", Value::from_bytes(typed_chain_id.underlying_chain_id().encode())),
+			("typed_chain_id", Value::u128(typed_chain_id.underlying_chain_id() as u128)),
 			("args", Value::from_bytes(init_input.encode()))
 		];
 		// Create a transaction to submit:
